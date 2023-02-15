922f8e753d4e2e54e45ac96ba9f33cb6b8e45cd4
danastasiev <danastasiev@alteryx.com>
2023-02-15 11:00:57 +0200

    add id count increment

--- nodejs-base/src/greeting/greeting.service.ts
+++ nodejs-base/src/greeting/greeting.service.ts
@@ -5,15 +5,17 @@ import {DEFAULT_NAME} from "./greeting.constants";
 @Service()
 export class GreetingService {
     private greetingTemplate: string;
+    private idCount: number;
 
     constructor() {
         this.greetingTemplate = process.env.GREETING || "Hello, %s!";
+        this.idCount = 1;
     }
 
     public getGreetingContent(name: string = DEFAULT_NAME): IGreetingContent {
         const content = this.greetingTemplate.replace('%s', name);
         return {
-            id: 1,
+            id: this.idCount++,
             content
         }
     }
