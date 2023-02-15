7952bd5429d3801eae4c164bf312997751478254
danastasiev <danastasiev@alteryx.com>
2023-02-15 16:57:03 +0200

    add default value for config

--- nodejs-base/src/config/config.reader.ts
+++ nodejs-base/src/config/config.reader.ts
@@ -1,13 +1,18 @@
 import {Service} from "typedi";
-import { propertiesToJson } from 'properties-file'
+import { propertiesToJson } from 'properties-file';
+import * as path from 'path';
+
 
 @Service()
 export default class ConfigReader {
     private configPath: string;
+    private config: any;
     constructor() {
-        this.configPath = process.env.CONFIG_LOCATION || ""
+        this.configPath = process.env.CONFIG_LOCATION ||
+            path.resolve(__dirname, `../../../kustomization/local.properties`);
+        this.config = propertiesToJson(this.configPath);
     }
     getAppConfig(): any{
-        return propertiesToJson(this.configPath)
+        return this.config;
     }
 }
